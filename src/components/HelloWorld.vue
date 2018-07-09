2b4d14fcbd0e9c1067c78a1bc27dc6a545dfcd28
Bohdan_Nosachenko <Bohdan_Nosachenko@epam.com>
2018-07-09 11:10:53 +0300

    the vue application is created

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <FirstComponent happy="happyness is here!"></FirstComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import FirstComponent from "./firstComoinent.vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    FirstComponent
+  },
+  props: {
+    msg: String
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/firstComoinent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>The first vue component is created! {{ happy }}</div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  props: {
+    happy: String
+  }
+};
+</script>
